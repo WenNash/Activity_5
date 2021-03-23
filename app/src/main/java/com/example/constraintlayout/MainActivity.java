81017ae2ed4d1d1222032e3f9b83305b39a3e454
Wean Nashrullah <weannashrullah@gmail.com>
2021-03-23 21:32:07 +0700

    Activity_4

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.constraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(), Pendaftaran.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    Button btnlogin;
+    EditText edEmail, edPassword;
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnlogin = findViewById(R.id.edBtnSignIn);
+        edEmail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = edEmail.getText().toString();
+                password = edPassword.getText().toString();
+
+                String email = "admin@mail.com";
+
+                String pass = "123";
+
+                if (nama.isEmpty() || password.isEmpty()) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan Password wajib di isi!", Toast.LENGTH_LONG);
+
+                    t.show();
+                } else {
+
+                    if (nama.equals(email) && password.equals(pass)) {
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses", Toast.LENGTH_LONG);
+                        t.show();
+                        Bundle b = new Bundle();
+
+                        b.putString("a", nama.trim());
+
+                        b.putString("b", password.trim());
+
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        i.putExtras(b);
+
+                        startActivity(i);
+                    } else {
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Galal", Toast.LENGTH_LONG);
+
+                        t.show();
+                    }
+                }
+            }
+        });
     }
 }
